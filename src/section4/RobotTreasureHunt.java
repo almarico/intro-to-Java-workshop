e82c77a3fb25168ff4d62187f83e58f757b1c304
administrator <administrator@172.24.2.64>
2022-07-29 14:26:36 -0700

    Robot treasure hunt and rocket

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,44 +16,54 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot mini = new Robot ("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		mini.setAngle(0);
+		mini.microMove(5);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		mini.setAngle(180);
+		mini.microMove(5);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+mini.setAngle(270);
+mini.microMove(5);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		mini.setAngle(90);
+		mini.microMove(5);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = mini.getX();
+		int robotYLocation = mini.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println("robot x location is " + robotXLocation + " robot y location is " + robotYLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
-		
+		if (robotYLocation >=400 && robotYLocation <=465 && robotXLocation >= 710 && robotXLocation <=740) {
+		JOptionPane.showMessageDialog(null, "go in the skulls eye");
+		}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
-		
+		if (robotYLocation >=35 && robotYLocation <=65 && robotXLocation >=170 && robotXLocation <=230) {
+		JOptionPane.showMessageDialog(null, "find the hole in ther ground");
+		}
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
-		
+		if (robotYLocation>= 470 && robotYLocation <=485 && robotXLocation >= 235 && robotXLocation<=285) {
+			treasureFound(); 
+		}
 	}
 
 	private void go() {
--- src/zebra/section1/rocket/rocket.pde
+++ src/zebra/section1/rocket/rocket.pde
@@ -4,20 +4,38 @@ int rocketY = 600;
 void setup() {
   background(0, 0, 40);
   size(800, 800);
+    background(0, 0, 0);
+  fill(255, 255, 255);
+  circle(random(width),random(height),random(15));
+    circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+    circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+  circle(random(width),random(height),random(15));
+
+
 }
 
 void draw() {
-  background(0, 0, 0);
-  fill(255, 255, 255);
   // 1. ▼ Write code BELOW this line to make stars ▼ 
   //    Example:
-  //     circle(50, 50, 50);
+  
+  
 
 
 
 
   
-
   fill(random(255), 0, 0);
   circle(rocketX, rocketY + 130, 90);
   fill(248, 128, 0);
@@ -27,7 +45,8 @@ void draw() {
   fill(100, 100, 100);
   triangle(rocketX, rocketY + 10, rocketX + 50, rocketY + 100, rocketX - 50, rocketY + 100);
   // 2. ▼ Write code BELOW this line to make the rocket blast off ▼ 
-    
+    rocketY -=1;
+   
  
 
 
